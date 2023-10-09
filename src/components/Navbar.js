2499dae57a6018493d5a6ba962c5de594da7d28e
Edwin <edoumacodu@gmail.com>
2023-10-09 22:38:34 +0300

    created navbar and styled using css

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h2> Blog Post</h2>
+            <div className="links">
+                <a href="#"> Home</a>
+                <a href="#"> new blog</a>
+            </div>
+            
+        </nav>
+     );
+}
+ 
+export default Navbar;
